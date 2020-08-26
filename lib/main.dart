2a157d09555cb40a1b657cfa29689294220ded17
SidM9 <43111742+Sidm9@users.noreply.github.com>
2020-08-26 13:16:49 +0530

    Removed Boilerplate code and added font

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "This is awsome!!!",
+            style: TextStyle(
+              fontFamily: 'ProductSans',
+              fontSize: 20,
+              // fontWeight: FontWeight.bold,
+              // color: Colors.grey[600],
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Text('hellossqq'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("Clik"),
+        ),
       ),
-    ),
-  );
-}
+    ));
+
